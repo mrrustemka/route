41d091db545cd43fe6f2b88ac3219a5930012a5e
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-09-24 16:10:36 +0300

    fix-tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-// import { ProductsPage } from "./pages/ProductsPage";
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-black">
       <Header />
       <Outlet />
     </div>
--- src/Header.tsx
+++ src/Header.tsx
@@ -3,14 +3,14 @@ import logo from "./logo.svg";
 
 export function Header() {
   return (
-    <header className="text-center text-slate-50       bg-slate-900 h-40 p-5">
+    <header className="text-center text-slate-50 bg-slate-900 h-40 p-5 bg-black">
       <img src={logo} alt="Logo" className="inline-block h-20" />
-      <h1 className="text-2xl">React Tools</h1>
+      <h1 className="text-2xl bg-black">React Tools</h1>
       <nav>
         <NavLink
           to="products"
           className={({ isActive }) =>
-            `text-white no-underline p-1 pb-0.5 border-solid        border-b-2 ${
+            `text-white no-underline p-1 pb-0.5 border-solid border-b-2 ${
               isActive ? "border-white" : "border-transparent"
             }`
           }
